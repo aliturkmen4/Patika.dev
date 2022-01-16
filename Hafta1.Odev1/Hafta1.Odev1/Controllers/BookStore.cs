2d6d7275f495fc4092c9bfa05e5e3474866b9966
Ali Türkmen <84051961+aliturkmen4@users.noreply.github.com>
2022-01-16 10:21:46 +0300

    Add files via upload

---
+++ Hafta1.Odev1/Hafta1.Odev1/Controllers/BookStore.cs
@@ -0,0 +1,48 @@
+﻿using Hafta1.Odev1.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hafta1.Odev1.Controllers
+{
+    [Route("api/bookstore")]
+    [ApiController]
+    public class BookStore : ControllerBase
+    {
+        private object rbook;
+
+        [HttpGet("{bookname}/{id}")] //api/library/{bookname}/{id}
+        public IActionResult GetbyId(string bookname, int id)
+        {
+            return Ok(new { bookname = bookname, id = id }); //return 200 status code
+        }
+        [HttpPost]
+        public IActionResult Create([FromBody] Book book)
+        {
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult Patch(int id, [FromBody] Book book)
+        {
+            if (rbook != null)
+            {
+                book.ApplyTo(rbook);
+                return NotFound();
+            }
+            return Ok();
+        }
+        [HttpPost("{id}")] 
+        public IActionResult CreatedI([FromQuery] Book book)
+        {
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Deleted(int id,[FromBody] Book book)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ Hafta1.Odev1/Hafta1.Odev1/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Hafta1.Odev1.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int GenreId { get; set; }
+
+        public int PageCount { get; set; }
+
+        internal void ApplyTo(object rbook)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
